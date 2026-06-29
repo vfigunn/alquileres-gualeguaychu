08d32db5a086446f246c2f7509b7d5336daa19cd
vfigun <vfigun@gmail.com>
2026-06-29 11:01:04 -0300

    feat(layout): aumentar max-width a 2000px para tarjetas más grandes

--- app/layout.tsx
+++ app/layout.tsx
@@ -107,7 +107,7 @@ export default function RootLayout({
         {/* TopNavBar */}
         <TopNavBar />
 
-        <main className="flex-grow pt-[130px] max-w-[1800px] mx-auto w-full px-gutter flex gap-lg pb-xl overflow-x-hidden">
+        <main className="flex-grow pt-[130px] max-w-[2000px] mx-auto w-full px-gutter flex gap-lg pb-xl overflow-x-hidden">
           {children}
         </main>
 
